5a4f4de100306fc1f13ffa3b6f7e11b471bee9c0
tianxie <txie@live.cn>
2022-05-15 21:26:03 +0800

    web-server

--- web-server/server.py
+++ web-server/server.py
@@ -8,18 +8,41 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     Page = '''\
 <html>
 <body>
-<p>Hello, web!</p>
+<table>
+<tr>  <td>Header</td>         <td>Value</td>          </tr>
+<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+<tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+<tr>  <td>Command</td>        <td>{command}</td>      </tr>
+<tr>  <td>Path</td>           <td>{path}</td>         </tr>
+</table>
 </body>
 </html>
 '''
 
     # Handle a GET request.
     def do_GET(self):
+        page = self.create_page()
+        self.send_page(page)
+
+    def send_page(self, page):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
+        self.send_header("Content-Length", str(len(page)))
         self.end_headers()
-        self.wfile.write(self.Page.encode('utf-8'))
+        self.wfile.write(page.encode('utf-8'))
+
+    def create_page(self):
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port': self.client_address[1],
+            'command': self.command,
+            'path': self.path
+        }
+
+        page = self.Page.format_map(values)
+        return page
 
 
 # -------------------------------------------------------------------------------
