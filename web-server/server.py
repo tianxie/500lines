0fb52fa2f5fa3887f387058302daef78bff5e8f8
tianxie <txie@live.cn>
2022-05-15 22:33:42 +0800

    web-server

--- web-server/server.py
+++ web-server/server.py
@@ -1,48 +1,55 @@
 import http.server
+import os
+
+
+class ServerException(Exception):
+    pass
 
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     '''Handle HTTP requests by returning a fixed 'page'.'''
 
-    # Page to send back.
-    Page = '''\
-<html>
-<body>
-<table>
-<tr>  <td>Header</td>         <td>Value</td>          </tr>
-<tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-<tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-<tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
-<tr>  <td>Command</td>        <td>{command}</td>      </tr>
-<tr>  <td>Path</td>           <td>{path}</td>         </tr>
-</table>
-</body>
-</html>
-'''
+    Error_Page = """\
+            <html>
+            <body>
+            <h1>Error accessing {path}</h1>
+            <p>{msg}</p>
+            </body>
+            </html>
+            """
 
     # Handle a GET request.
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        full_path = os.getcwd() + os.path.normpath(self.path)
+        try:
+            if not os.path.exists(full_path):
+                raise ServerException('{0} not found'.format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+        except ServerException as e:
+            self.handle_error(e)
 
-    def send_page(self, page):
-        self.send_response(200)
+    def send_content(self, content, status=200):
+        self.send_response(status)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
+        self.send_header("Content-Length", str(len(content)))
         self.end_headers()
-        self.wfile.write(page.encode('utf-8'))
+        self.wfile.write(content)
 
-    def create_page(self):
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path
-        }
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as f:
+                content = f.read()
+                self.send_content(content)
+        except IOError as e:
+            msg = "'{0}' cannot be read: {1}".format(self.path, e)
+            self.handle_error(msg)
 
-        page = self.Page.format_map(values)
-        return page
+    def handle_error(self, e):
+        content = self.Error_Page.format(path=self.path, msg=e)
+        self.send_content(content.encode('utf-8'), 404)
 
 
 # -------------------------------------------------------------------------------
