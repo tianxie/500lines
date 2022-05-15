e2cb350a62e25272c1feff074162e44842801f8f
tianxie <txie@live.cn>
2022-05-15 21:16:55 +0800

    web-server

---
+++ web-server/server.py
@@ -0,0 +1,30 @@
+import http.server
+
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    '''Handle HTTP requests by returning a fixed 'page'.'''
+
+    # Page to send back.
+    Page = '''\
+<html>
+<body>
+<p>Hello, web!</p>
+</body>
+</html>
+'''
+
+    # Handle a GET request.
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page.encode('utf-8'))
+
+
+# -------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    server_address = ('', 8080)
+    server = http.server.HTTPServer(server_address, RequestHandler)
+    server.serve_forever()
