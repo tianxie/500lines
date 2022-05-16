83104237b2c25b90a274b423b1b77414c911e49d
tianxie <txie@live.cn>
2022-05-16 23:14:05 +0800

    web-server

--- web-server/server.py
+++ web-server/server.py
@@ -6,8 +6,67 @@ class ServerException(Exception):
     pass
 
 
+class case_no_file:
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise ServerException("'{0}' not found".format(handler.path))
+
+
+class case_existing_file:
+    '''File exists.'''
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+
+class case_always_fail:
+    '''Base case if nothing else worked.'''
+
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise ServerException("Unknown object '{0}'".format(handler.path))
+
+
+class case_directory_index_file:
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+               os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.handle_file(self.index_path(handler))
+
+
+class case_directory_no_index_file(object):
+    '''Serve listing for a directory without an index.html page.'''
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+               not os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.list_dir(handler.full_path)
+
+
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-    '''Handle HTTP requests by returning a fixed 'page'.'''
+    Cases = [case_no_file(),
+             case_existing_file(),
+             case_directory_index_file(),
+             case_directory_no_index_file(),
+             case_always_fail()]
 
     Error_Page = """\
             <html>
@@ -18,16 +77,25 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             </html>
             """
 
+    Listing_Page = '''\
+            <html>
+            <body>
+            <ul>
+            {0}
+            </ul>
+            </body>
+            </html>
+            '''
+
     # Handle a GET request.
     def do_GET(self):
-        full_path = os.getcwd() + os.path.normpath(self.path)
+        self.full_path = os.getcwd() + os.path.normpath(self.path)
+
         try:
-            if not os.path.exists(full_path):
-                raise ServerException('{0} not found'.format(self.path))
-            elif os.path.isfile(full_path):
-                self.handle_file(full_path)
-            else:
-                raise ServerException("Unknown object '{0}'".format(self.path))
+            for case in self.Cases:
+                if case.test(self):
+                    case.act(self)
+                    break
         except ServerException as e:
             self.handle_error(e)
 
@@ -50,6 +118,16 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     def handle_error(self, e):
         content = self.Error_Page.format(path=self.path, msg=e)
         self.send_content(content.encode('utf-8'), 404)
+
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page.encode('utf-8'))
+        except OSError as e:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, e)
+            self.handle_error(msg)
 
 
 # -------------------------------------------------------------------------------
